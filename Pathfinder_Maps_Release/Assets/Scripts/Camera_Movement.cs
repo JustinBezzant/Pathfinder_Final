559a89eb2c862bb4dc8158bcce4d13521fae59db
JustinBezzant <justinbezzant@gmail.com>
2020-08-09 14:42:03 -0600

    First Floor Built

---
+++ Pathfinder_Maps_Release/Assets/Scripts/Camera_Movement.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour
+{
+    private float transVelUp;
+    private float transVelDown;
+    private float transVelLeft;
+    private float transVelRight;
+    public float transAccel;
+    public float maxSpeed;
+    public float maxZ, minZ,maxX,minX;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (gameObject.transform.position.z < maxZ)
+            {
+                transVelUp = transVelUp + transAccel;
+                if (transVelUp > maxSpeed)
+                {
+                    transVelUp = maxSpeed;
+                }
+                gameObject.transform.Translate(0, transVelUp, 0);
+            }
+        }
+        else
+        {
+            transVelUp = 0;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (gameObject.transform.position.z > minZ)
+            {
+
+                transVelDown = transVelDown - transAccel;
+                if (transVelDown > maxSpeed)
+                {
+                    transVelDown = maxSpeed;
+                }
+                gameObject.transform.Translate(0, transVelDown, 0);
+            }
+        }
+        else
+        {
+            transVelDown = 0;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (gameObject.transform.position.x > minX)
+            {
+                transVelLeft = transVelLeft - transAccel;
+                if (transVelLeft > maxSpeed)
+                {
+                    transVelLeft = maxSpeed;
+                }
+                gameObject.transform.Translate(transVelLeft, 0, 0);
+            }
+        }
+        else
+        {
+            transVelLeft = 0;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (gameObject.transform.position.x < maxX)
+            {
+                transVelRight = transVelRight + transAccel;
+                if (transVelRight > maxSpeed)
+                {
+                    transVelRight = maxSpeed;
+                }
+                gameObject.transform.Translate(transVelRight, 0, 0);
+            }
+        }
+        else
+        {
+            transVelRight = 0;
+        }
+    }
+}
---
+++ Pathfinder_Maps_Release/Assets/Scripts/Camera_Scroll.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Scroll : MonoBehaviour
+{
+    public float maxZoom;
+    public float maxZoomOut;
+    public float scrollSpeed;
+    private float cameraMove;
+    private Vector3 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            Debug.Log("hi");
+            cameraMove = -1f * scrollSpeed;
+            if (transform.position.y < maxZoomOut)
+            {
+                transform.Translate(new Vector3(0, 0, -1 * scrollSpeed));
+            }
+        }
+        else if (Input.mouseScrollDelta.y > 0)
+        {
+            cameraMove = scrollSpeed;
+            if (transform.position.y > maxZoom)
+            {
+                transform.Translate(new Vector3(0, 0, scrollSpeed));
+            }
+        }
+    }
+}
